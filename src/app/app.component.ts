498ccfb89bbb65bae726975595af85e17675320c
Florian Tischler <florian.tischler@gmx.net>
2019-02-15 15:43:10 +0100

    WebSocket sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ws-sample';
+export class AppComponent implements OnInit {
+  constructor(private socket: Socket) { }
+
+  ngOnInit(): void {
+    this.socket.fromEvent('connect').subscribe(() => {
+      this.socket.emit('Ping', 'ping');
+    });
+
+    this.socket.fromEvent('Test').subscribe(ev => console.log('Test', ev));
+    this.socket.fromEvent('Pong').subscribe(ev => console.log('Pong', ev));
+    this.socket.fromEvent('UserJoined').subscribe(ev => console.log('UserJoined', ev));
+    this.socket.fromEvent('UserLeft').subscribe(ev => console.log('UserLeft', ev));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Socket, SocketIoModule } from 'ngx-socket-io';
+
+export function socketFactory(): Socket {
+  const token: string | undefined = localStorage.getItem('token');
+  const options = token ? { query: `token=${token}` } : {};
+
+  return new Socket({
+    url: '/', options: {
+      ...options,
+      transports: ['websocket']
+    }
+  });
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule
   ],
-  providers: [],
+  providers: [
+    { provide: Socket, useFactory: socketFactory }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
